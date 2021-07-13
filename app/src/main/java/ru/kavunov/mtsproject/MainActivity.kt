d7a43a9d24e0526a6bab239e4f2700de7297912b
KavunovAndrei <fish505@mail.ru>
2021-07-13 16:48:44 +0300

    ДЗ 3 версия 1.2

--- app/src/main/java/ru/kavunov/mtsproject/MainActivity.kt
+++ app/src/main/java/ru/kavunov/mtsproject/MainActivity.kt
@@ -3,19 +3,23 @@ package ru.kavunov.mtsproject
 import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import ru.kavunov.mtsproject.adapter.CategoryAdapter
+import ru.kavunov.mtsproject.adapter.MovieAdapter
 import ru.kavunov.mtsproject.databinding.ActivityMovieDetailsBinding
 
 
 class MainActivity : AppCompatActivity() {
     private val adapterCateg = CategoryAdapter()
+    private val adapterMovie = MovieAdapter()
     lateinit var binding: ActivityMovieDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init1()
+        init2()
 
     }
 
@@ -24,6 +28,14 @@ class MainActivity : AppCompatActivity() {
         binding.RcCateg.adapter = adapterCateg
         for(i in adapterCateg.list1){
             adapterCateg.categtList.add(i)
+        }
+
+    }
+    fun init2(){
+        binding.RcMovie.layoutManager = GridLayoutManager(this, 2)
+        binding.RcMovie.adapter = adapterMovie
+        for(i in adapterMovie.list2){
+            adapterMovie.movietList.add(i)
         }
 
     }
---
+++ app/src/main/java/ru/kavunov/mtsproject/adapter/MovieAdapter.kt
@@ -0,0 +1,101 @@
+package ru.kavunov.mtsproject.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import ru.kavunov.mtsproject.DTC.Categories
+import ru.kavunov.mtsproject.DTC.MovieDto
+import ru.kavunov.mtsproject.R
+import ru.kavunov.mtsproject.databinding.CategoryItemBinding
+import ru.kavunov.mtsproject.databinding.ItemMovieBinding
+
+class MovieAdapter(): RecyclerView.Adapter<MovieAdapter.MovieHolder>() {
+    var movietList = ArrayList<MovieDto>()
+    val list2 = listOf(
+        MovieDto(
+            title = "Гнев человеческий",
+            description = "Эйч — загадочный и холодный на вид джентльмен, но внутри него пылает жажда справедливости. Преследуя...",
+            rateScore = 3,
+            ageRestriction = 18,
+            imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5JP9X5tCZ6qz7DYMabLmrQirlWh.jpg"
+        ),
+        MovieDto(
+            title = "Мортал Комбат",
+            description = "Боец смешанных единоборств Коул Янг не раз соглашался проиграть за деньги. Он не знает о своем наследии...",
+            rateScore = 5,
+            ageRestriction = 18,
+            imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pMIixvHwsD5RZxbvgsDSNkpKy0R.jpg"
+        ),
+        MovieDto(
+            title = "Упс... Приплыли!",
+            description = "От Великого потопа зверей спас ковчег. Но спустя полгода скитаний они готовы сбежать с него куда угодно...",
+            rateScore = 5,
+            ageRestriction = 6,
+            imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/546RNYy9Wi5wgboQ7EtD6i0DY5D.jpg"
+        ),
+        MovieDto(
+            title = "The Box",
+            description = "Уличный музыкант знакомится с музыкальным продюсером, и они вдвоём отправляются в путешествие...",
+            rateScore = 4,
+            ageRestriction = 12,
+            imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fq3DSw74fAodrbLiSv0BW1Ya4Ae.jpg"
+        ),
+        MovieDto(
+            title = "Сага о Дэнни Эрнандесе",
+            description = "Tekashi69 или Сикснайн — знаменитый бруклинский рэпер с радужными волосами — прогремел...",
+            rateScore = 2,
+            ageRestriction = 18,
+            imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5xXGQLVtTAExHY92DHD9ewGmKxf.jpg"
+        ),
+        MovieDto(
+            title = "Пчелка Майя",
+            description = "Когда упрямая пчелка Майя и ее лучший друг Вилли спасают принцессу-муравьишку, начинается сказочное...",
+            rateScore = 4,
+            ageRestriction = 0,
+            imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xltjMeLlxywym14NEizl0metO10.jpg"
+        ),
+        MovieDto(
+            title = "Круэлла",
+            description = "Невероятно одаренная мошенница по имени Эстелла решает сделать себе имя в мире моды.",
+            rateScore = 4,
+            ageRestriction = 12,
+            imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hUfyYGP9Xf6cHF9y44JXJV3NxZM.jpg"
+        ),
+        MovieDto(
+            title = "Чёрная вдова",
+            description = "Чёрной Вдове придется вспомнить о том, что было в её жизни задолго до присоединения к команде Мстителей",
+            rateScore = 3,
+            ageRestriction = 16,
+            imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mbtN6V6y5kdawvAkzqN4ohi576a.jpg"
+        ),
+    )
+    class MovieHolder(item:View):RecyclerView.ViewHolder(item) {
+        val building = ItemMovieBinding.bind(item)
+        fun bind(movie: MovieDto){building.apply {
+            filmImg.load(R.drawable.oval2)
+            filmName.text = movie.title
+            filmContent.text = movie.description
+            filmOgr.text = movie.ageRestriction.toString()
+            filmRating.rating = movie.rateScore.toFloat()
+        }
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        return MovieAdapter.MovieHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+        holder.bind(movietList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return movietList.size
+    }
+}
