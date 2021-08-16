712bf64c0c7a175ebed93da7ed425563d93a9834
KavunovAndrei <fish505@mail.ru>
2021-08-16 14:42:13 +0300

    9.3

--- app/src/main/java/ru/kavunov/mtsproject/MainActivity.kt
+++ app/src/main/java/ru/kavunov/mtsproject/MainActivity.kt
@@ -18,6 +18,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.*
+import okio.IOException
 
 import ru.kavunov.mtsproject.databinding.ActivityMovieDetailsBinding
 import ru.kavunov.mtsproject.mvvm.model.*
@@ -41,7 +43,7 @@ class MainActivity : AppCompatActivity(), MovieClickListener {
         val navController = navHostFragment.navController
         findViewById<BottomNavigationView>(R.id.BoNav)
             .setupWithNavController(navController)
-
+        okHTTPonly()
 
     }
 
@@ -53,6 +55,42 @@ class MainActivity : AppCompatActivity(), MovieClickListener {
 
 }
 
+fun okHTTPonly(){
+//    var x: String = "ss"
+    val client = OkHttpClient()
+    val request = Request.Builder()
+//            .url("https://www.simplifiedcoding.net/demos/marvel/")
+            .url("https://api.themoviedb.org/3/movie/popular/550?api_key=b62341778732f78e2661370039f79b84")
+
+//            .url("https://api.themoviedb.org/3/movie/550?api_key=b62341778732f78e2661370039f79b84")
+
+        .build()
+
+//            client.newCall(request).execute().use { response ->
+//                if (!response.isSuccessful) throw IOException("Unexpected code $response")
+//                val result = response.body!!.string()
+
+    client.newCall(request).enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {
+            e.printStackTrace()
+        }
+
+        override fun onResponse(call: Call, response: Response){
+            response.use {
+                try {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    val result = response.body?.string() ?: "error"
+//                        x = result
+                    Log.d("tag11", result.toString())
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+        }
+    })
+
+}
 
 
 
